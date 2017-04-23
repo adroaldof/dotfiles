8a924b9ed9b6f956b362389de26cecf9cda69a0d
Adroaldo de Andrade <adroaldof@gmail.com>
2017-04-22 23:02:13 -0300

    Show intallation messages

--- setup-machine.sh
+++ setup-machine.sh
@@ -1,53 +1,47 @@
-##############################################################################
-# Generate ssh keys
-###############################################################################
-# ssh-keygen -t rsa -b 4096 -C "your@email.com"
-# chmod 600 ~/.ssh/*
-echo "********************************************************************************"
-echo ""
+echo "###############################################################################"
+echo "# Generate ssh keys"
+echo "###############################################################################"
+echo "# Please enter your email:"
+read email
+ssh-keygen -t rsa -N -b 4096 -C $email
+chmod 600 ~/.ssh/*
 
 
-###############################################################################
-# Make all updates
-###############################################################################
-echo "Updating system"
+echo "###############################################################################"
+echo "# Make all system updates"
+echo "###############################################################################"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
 
-###############################################################################
-# Install Essential Packages
-###############################################################################
-echo "Installing essentials packages"
+echo "###############################################################################"
+echo "# Install Essential Packages"
+echo "###############################################################################"
 sudo apt-get install build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip -y
 
 
-###############################################################################
-# Install Git
-###############################################################################
-echo "Installing Git"
+echo "###############################################################################"
+echo "# Install Git"
+echo "###############################################################################"
 sudo apt-get install git-core -y
 
 
-###############################################################################
-# Install Tweak Tools
-###############################################################################
-echo "Installing Tweak Tools"
+echo "###############################################################################"
+echo "# Install Tweak Tools"
+echo "###############################################################################"
 sudo apt-get install gnome-tweak-tool -y
 
 
-###############################################################################
-# Install Curl
-###############################################################################
-echo "Installing Curl"
+echo "###############################################################################"
+echo "# Install Curl"
+echo "###############################################################################"
 sudo apt install curl -y
 
 
-###############################################################################
-# Install vim
-###############################################################################
-echo "Installing Vim and its goods"
+echo "###############################################################################"
+echo "# Install vim and its goods"
+echo "###############################################################################"
 # install vim
 sudo apt-get install vim -y
 
@@ -58,9 +52,9 @@ sudo apt-get install vim-gtk -y
 sudo apt-get install silversearcher-ag -y
 
 
-###############################################################################
-# Install VSCode
-###############################################################################
+echo "###############################################################################"
+echo "# Install VSCode"
+echo "###############################################################################"
 curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > /tmp/vscode.deb
 sudo dpkg -i /tmp/vscode.deb && sudo apt-get install -f
 
@@ -86,9 +80,9 @@ code --install-extension wmaurer.change-case
 code --install-extension zhutian.swig
 
 
-###############################################################################
-# Install Atom
-###############################################################################
+echo "###############################################################################"
+echo "# Install Atom"
+echo "###############################################################################"
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update -y
 sudo apt-get install atom -y
@@ -126,10 +120,9 @@ apm install todo-show
 apm install visual-studio-dark-syntax
 apm install webbox-color
 
-###############################################################################
-# Install xClip
-###############################################################################
-echo "Installing xClip"
+echo "###############################################################################"
+echo "# Install xClip"
+echo "###############################################################################"
 echo "Add the follow aliases to your config file"
 echo "alias pbcopy='xclip -selection clipboard'"
 echo "alias pbpaste='xclip -selection clipboard -o'"
@@ -140,10 +133,9 @@ echo "alias pbpaste='xclip -selection clipboard -o'"
 sudo apt-get install xclip -y
 
 
-###############################################################################
-# Install NVM
-###############################################################################
-echo "Installing NVM"
+echo "###############################################################################"
+echo "# Install NVM"
+echo "###############################################################################"
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
@@ -152,10 +144,9 @@ nvm install stable
 npm i -g git-x
 
 
-###############################################################################
-# Install Docker
-###############################################################################
-echo "Installing Docker"
+echo "###############################################################################"
+echo "# Install Docker"
+echo "###############################################################################"
 # Install docker
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
@@ -172,19 +163,17 @@ curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 
-###############################################################################
-# Install ZSH and oh-my-zsh
-###############################################################################
-echo "Installing ZSH and Oh-my-zsh"
+echo "###############################################################################"
+echo "# Install ZSH and oh-my-zsh"
+echo "###############################################################################"
 sudo apt-get install zsh -y
 sh -c "$(wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh)"
 chsh -s `which zsh`
 
 
-###############################################################################
-# Install Tmux
-###############################################################################
-echo "Installing Tmux"
+echo "###############################################################################"
+echo "# Install Tmux"
+echo "###############################################################################"
 # Aliases
 # -----
 # alias tmux="tmux -2"
@@ -195,21 +184,21 @@ echo "Installing Tmux"
 sudo apt-get install tmux -y
 
 
-###############################################################################
-# Install Browsers
-###############################################################################
-echo "Installing Google Chrome"
+echo "###############################################################################"
+echo "# Install Browsers"
+echo "###############################################################################"
+echo "# Installing Google Chrome"
 # Install Google Chrome Browser
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update -y
 sudo apt-get install google-chrome-stable -y
 
-echo "Installing Google Chromium"
+echo "# Installing Google Chromium"
 # Install Chromium Browser
 sudo apt-get install chromium-browser -y
 
-echo "Installing Opera"
+echo "# Installing Opera"
 # Install Opera browser
 sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
 sudo sh -c 'wget -O - http://deb.opera.com/archive.key | apt-key add -'
@@ -217,33 +206,31 @@ sudo apt-get update -y
 sudo apt-get install opera -y
 
 
-###############################################################################
-# Install SQLEletrom
-###############################################################################
-echo "Installing SQLEletrom"
-curl -L https://github.com/sqlectron/sqlectron-gui/releases/download/v1.17.0/Sqlectron-1.20.0.deb > /tmp/sqlectron.deb
+echo "###############################################################################"
+echo "# Install SQLEletrom"
+echo "###############################################################################"
+curl -L https://github.com/sqlectron/sqlectron-gui/releases/download/v1.17.0/Sqlectron-1.17.0.deb > /tmp/sqlectron.deb
 sudo dpkg -i /tmp/sqlectron.deb && sudo apt install -f
 
 
-###############################################################################
-# Install Spotify
-###############################################################################
-echo "Installing Spotify"
+echo "###############################################################################"
+echo "# Install Spotify"
+echo "###############################################################################"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 sudo apt-get install spotify-client
 
 
-###############################################################################
-# Install Slack
-###############################################################################
+echo "###############################################################################"
+echo "# Install Slack"
+echo "###############################################################################"
 https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb > /tmp/slack.deb
 sudo dpkg -i /tmp/slack.deb && sudo apt install -f
 
-###############################################################################
-# Post Install Setups
-###############################################################################
+echo "###############################################################################"
+echo "# Post Install Setups"
+echo "###############################################################################"
 # Clean packages
 sudo apt-get clean
 
