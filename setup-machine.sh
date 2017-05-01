f315d039e21270a7bcfe90bca1353090fb627107
Adroaldo de Andrade <adroaldof@gmail.com>
2017-05-01 13:12:12 -0300

    update nvm install

--- setup-machine.sh
+++ setup-machine.sh
@@ -149,7 +149,17 @@ sudo apt-get install xclip -y
 echo "###############################################################################"
 echo "# Install NVM"
 echo "###############################################################################"
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+echo ". ~/.nvm/nvm.sh" >> ~/.bashrc
+echo ". ~/.nvm/nvm.sh" >> ~/.zshrc
+source ~/.nvm/nvm.sh
+nvm install stable #or nvm install <version>
+nvm use stable
+n=$(which node);n=${n%/bin/node}; sudo chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
+
+sudo chmod g+w /usr/local/lib/node_modules
+sudo chmod g+w /usr/bin/npm
+sudo chmod g+w /usr/local/bin/npm
 
 
 echo "###############################################################################"
